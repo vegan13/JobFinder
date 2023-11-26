c4747d5dafefabd1aa03b9320e635f5b032e5704
vegan13 <vegan13@o2.pl>
2023-11-26 12:53:00 +0100

    Add project files.

---
+++ JobFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
